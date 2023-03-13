f0894292af63456018fd58a3350320c56e0517f0
Ansh Naikele <ansh.naikele2003@gmail.com>
2023-03-13 00:27:29 -0500

    formatting changes

--- Frontend/src/pages/Welcome.js
+++ Frontend/src/pages/Welcome.js
@@ -4,34 +4,21 @@ import './Welcome.css';
 function Welcome() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */
-        <div className = "nameEnc">
-          <label for="nameField" id="name">Syllabye</label>
-          </div>
-        }
-      </header>
-      <div className = "Form">
-        <div className = "RealForm">
-          <label for="signinField" id="signin">Welcome to Syllabye</label>
-          <p for="welcoming" id = "welcomeMessage">The web app that will organize your classes for you. Sign up to get started.</p>
-          <div className = "Buttons">
-            <button type="button" id="signinButton">Sign in</button>
-            <button type="button" id="signupButton">Sign up</button>
-          </div>
+        <header className="App-header">
+            <div className = "nameEnc">
+                <label for="nameField" id="name">Syllabye</label>
+            </div>
+        </header>
+        <div className = "Form">
+            <div className = "RealForm">
+                <label for="signinField" id="signin">Welcome to Syllabye</label>
+                <p for="welcoming" id = "welcomeMessage">The web app that will organize your classes for you. Sign up to get started.</p>
+                <div className = "Buttons">
+                    <input type="submit" value="Sign In"/>
+                    <input type="submit" value="Sign Up"/>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
   );
 }
