3d544edbd6fead2c1a712b94c0c4a057b0b8ad53
Abel Thomas <abugthomas@gmail.com>
2023-03-12 21:58:18 -0500

    PDF parser

--- Backend/models/file.js
+++ Backend/models/file.js
@@ -4,5 +4,6 @@ const File = new mongoose.Schema(
     {
         email: {type: String, required: true},
         pdf: {type: Object, required: true},
+        coursenum: {type: String, required: true},
     }
 )
